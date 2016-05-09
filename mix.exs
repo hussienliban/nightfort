8dec88ef41bc7a0eb607d5adadce93ad438b0e3e
Hussien Liban <mntstyle@gmail.com>
2016-05-09 08:59:55 +0300

    Adding deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,13 @@ defmodule NightFort.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: [
+        :logger,
+        :httpoison
+      ],
+      mod: { NightFort, [] }
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +33,9 @@ defmodule NightFort.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.8.3"},
+      {:poison, "~> 2.1"}
+    ]
   end
 end
